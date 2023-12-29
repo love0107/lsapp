5f085423120d87e8d74a9cb273c808891cee62ac
Lovlesh <lovlesh@FIN-IT-LT-791-MAC.local>
2023-12-29 14:58:08 +0530

    move to backend

--- backend/model/model_init.go
+++ backend/model/model_init.go
@@ -1,7 +1,7 @@
 package model
 
 import "github.com/beego/beego/orm"
-
+// init all the tables 
 func InitModel() {
 	orm.RegisterModel(new(User))// user table
     orm.RegisterModel(new(LsConfig))// config table 
